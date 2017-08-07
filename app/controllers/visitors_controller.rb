3438b37be4a653f69282c875712ef7866cfa2c07
Adam Derewecki <derewecki@gmail.com>
2017-08-06 17:36:45 -0700

    Add /recipes/chili

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,4 @@
 class VisitorsController < ApplicationController
+  def recipe_chili
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
+
+  get '/recipes/chili' => 'visitors#recipe_chili'
 end
