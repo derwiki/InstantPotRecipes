8a0d0ad4451d17b12e1fa066e69aa78fcaa129df
Adam Derewecki <derewecki@gmail.com>
2017-08-17 22:42:23 -0700

    Add chicken tikka masala

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,4 +1,7 @@
 class VisitorsController < ApplicationController
   def recipe_chili
   end
+
+  def recipe_chicken_tikka_masala
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
 
   get '/recipes/chili' => 'visitors#recipe_chili'
+  get '/recipes/chicken-tikka-masala' => 'visitors#recipe_chicken_tikka_masala'
 end
